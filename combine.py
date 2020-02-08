dcd4d8fd03176006fc99c7cd332230020c6c29af
kouta <k141303@edu.tut.ac.jp>
2020-02-09 00:25:37 +0900

    add ja_pageid

--- combine.py
+++ combine.py
@@ -1,6 +1,7 @@
 import json
 import tarfile
 import bz2
+from collections import defaultdict
 from multiprocessing import Pool
 import multiprocessing as multi
 
@@ -42,17 +43,18 @@ def load_langlink(langlink,lang):
 
 
 def create_training(langlink_dict,jaID_2_ene):
-    result = {}
+    result = defaultdict(list)
     print('Creating a training data ...')
-    for pair in langlink_dict:
-        if pair[0] in jaID_2_ene:
-            enes = jaID_2_ene[pair[0]]
-            new_id = pair[1]['pageid']
-            new_title = pair[1]['title']
-            result.setdefault(pair[1]['lang'],[])
-            result[pair[1]['lang']].append({'pageid':new_id,'title':new_title,'ENEs':enes})
+    for source_id, destination in langlink_dict:
+        enes = jaID_2_ene.get(source_id)
+        if enes is None:
+            continue
+        d = {'pageid':destination['pageid'],
+             'ja_pageid':source_id,
+             'title':destination['title'],
+             'ENEs':enes}
+        result[destinations['lang']].append(d)
     return result
-
 
 def load_data(ene_jawiki,langlink,lang=None,output=None):
     jaID_2_ene = create_id_dict(ene_jawiki)
