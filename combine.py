e7a02ef8582d10c7e83456dc5c0deb329276445e
kouta <k141303@edu.tut.ac.jp>
2020-02-09 00:15:14 +0900

    support multiprocessing

--- combine.py
+++ combine.py
@@ -1,6 +1,8 @@
 import json
 import tarfile
 import bz2
+from multiprocessing import Pool
+import multiprocessing as multi
 
 def output_json(training,lang,out_path):
     if lang != 'LIST':
@@ -15,35 +17,29 @@ def output_json(training,lang,out_path):
                     f.write(json.dumps(line)+'\n')
                 f.close()
 
-
 def create_id_dict(ene_jawiki):
     id_dict = {}
-    with tarfile.open(ene_jawiki) as taritems:
+    with tarfile.open(ene_jawiki) as taritems, Pool(multi.cpu_count()) as p:
         for member in taritems.getmembers():
-            if '.json' in member.name:
-                print('Opening {} ...'.format(member.name))
-                f_item = taritems.extractfile(member)
-                for line in f_item:
-                    j = json.loads(line)
-                    id_dict[j['pageid']] = j['ENEs']
+            if '.json' not in member.name:
+                continue
+            print('Opening {} ...'.format(member.name))
+            f_item = taritems.extractfile(member)
+            for j in p.imap_unordered(json.loads, f_item):
+                id_dict[j['pageid']] = j['ENEs']
     return id_dict
 
 def load_langlink(langlink,lang):
     result = []
-    with bz2.BZ2File(langlink) as f:
+    with bz2.BZ2File(langlink) as f, Pool(multi.cpu_count()) as p:
         print('Opening {} ...'.format(langlink))
-        for line in f:
-            j = json.loads(line)
-            if j['source']['lang'] == 'ja':
-                if lang:
-                    if check_ja_to_specific(j,lang):
-                        result.append([j['source']['pageid'],j['destination']])
-                else:
-                    result.append([j['source']['pageid'],j['destination']])
+        for j in p.imap_unordered(json.loads, f):
+            if j['source']['lang'] != 'ja':
+                continue
+            if lang is None or j['destination']['lang'] == lang:
+                result.append([j['source']['pageid'],j['destination']])
     return result
 
-def check_ja_to_specific(j,lang):
-    return (j['destination']['lang'] == lang)
 
 def create_training(langlink_dict,jaID_2_ene):
     result = {}
@@ -61,15 +57,15 @@ def create_training(langlink_dict,jaID_2_ene):
 def load_data(ene_jawiki,langlink,lang=None,output=None):
     jaID_2_ene = create_id_dict(ene_jawiki)
     langlink_dict = load_langlink(langlink,lang)
+    assert False, False
 
     if __name__ == "__main__":
         print(jaID_2_ene)
 
     training = create_training(langlink_dict,jaID_2_ene)
+
     if not lang:
         lang = "LIST"
-    if output:
-        output_json(training,lang,output)
-    else:
-        output_json(training,lang,'ENEW_{}.json'.format(lang))
+    output = output if output is not None else 'ENEW_{}.json'.format(lang)
+    output_json(training,lang,output)
     return training
